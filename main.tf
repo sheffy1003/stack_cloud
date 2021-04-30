b76efd36901301d70165fdcbad317a010874a0c2
sheriff suara <suara.sheriff@gmail.com>
2021-04-30 12:57:49 -0400

    update module

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "clixxapp" {
 
 
 module "clixxebs" {
-    source    = source="github.com/sheffy1003/stack_cloud.git?ref=stack_modules/clixxebs"
+    source    ="github.com/sheffy1003/stack_cloud.git//stack_modules?ref=clixxebs"
     AWS_REGION = var.AWS_REGION
     PATH_TO_PRIVATE_KEY=var.PATH_TO_PRIVATE_KEY
     PATH_TO_PUBLIC_KEY=var.PATH_TO_PUBLIC_KEY
