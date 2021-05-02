8036c4de8427190216273fa9604a001c641cee43
sheriff suara <suara.sheriff@gmail.com>
2021-05-01 20:06:33 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+
+module "clixxapp" {
+    source    = "./modules/clixxapp"
+    AWS_REGION = var.AWS_REGION
+    PATH_TO_PRIVATE_KEY=var.PATH_TO_PRIVATE_KEY
+    PATH_TO_PUBLIC_KEY=var.PATH_TO_PUBLIC_KEY
+    AMIS=var.AMIS
+    DATABASE_USER=var.DATABASE_USER
+    DATABASE_PASSWORD=var.DATABASE_PASSWORD
+    RDS_ENDPOINT=var.RDS_ENDPOINT
+    DATABASE_NAME=var.DATABASE_NAME
+    AWS_SECRET_KEY=var.AWS_SECRET_KEY
+    AWS_ACCESS_KEY=var.AWS_ACCESS_KEY
+
+}
+
+/*
+module "clixxebs" {
+    #source    ="./modules/clixxebs"
+    source="github.com/sheffy1003/stack_cloud.git?ref=stack_modules/clixxebs"
+    AWS_REGION = var.AWS_REGION
+    PATH_TO_PRIVATE_KEY=var.PATH_TO_PRIVATE_KEY
+    PATH_TO_PUBLIC_KEY=var.PATH_TO_PUBLIC_KEY
+    AMIS=var.AMIS
+    ebs_name=var.ebs_name
+    mysubnet_id=var.mysubnet_id
+    AZ_ZONE=var.AZ_ZONE
+    DATABASE_USER=var.DATABASE_USER
+    DATABASE_PASSWORD=var.DATABASE_PASSWORD
+    RDS_ENDPOINT=var.RDS_ENDPOINT
+    DATABASE_NAME=var.DATABASE_NAME
+    AWS_SECRET_KEY=var.AWS_SECRET_KEY
+    AWS_ACCESS_KEY=var.AWS_ACCESS_KEY
+    
+}
+
+
+/*
+source="github.com/sheffy1003/stack_cloud.git?ref=stack_modules/clixxebs"*/
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "mysubnet_id" {
+  type = map(string)
+  default = {
+    "us-east-1a" = "subnet-b21974ed"
+    "us-east-1b" = "subnet-d5dabbb3"
+    "us-east-1c" = "subnet-2ca6c90d"
+    "us-east-1d" = "subnet-0f4c7c42"
+    "us-east-1e" = "subnet-ad9b2f9c"
+    "us-east-1f" = "subnet-3c144e32"
+  }
+}
+variable "ebs_name" {
+  default = {
+    ebs_1 = "/dev/sdb"
+    ebs_2 = "/dev/sdc"
+    ebs_3 = "/dev/sdd"
+  } 
+}
+
+variable "AZ_ZONE" {
+  default = "us-east-1a"
+}
+
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "MYLAMPKey"
+  sensitive = true
+}
+
+variable "directory" {
+  default = "/var/www/html"
+  
+}
+
+variable "RDS_ENDPOINT"  {}
+
+variable "DATABASE_NAME" {}
+
+variable "DATABASE_USER" {}
+
+variable "DATABASE_PASSWORD" {}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "modules/clixxebs/terra-key.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+   # us-east-1 = "ami-13be557e"
+    "us-east-1" = "ami-0742b4e673072066f"
+    "us-west-2" = "ami-06b94666"
+    "eu-west-1" = "ami-844e0bf7"
+  }
+}
+
