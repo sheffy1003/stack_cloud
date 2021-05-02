d8baf5c25803e9ee87a609ecc34f684e4e8d098d
sheriff suara <suara.sheriff@gmail.com>
2021-05-01 20:15:41 -0400

    initial commit

---
+++ clixxapp/output.tf
@@ -0,0 +1,16 @@
+
+#DISPLAY PUBLIC IP ADDRESS
+output "public_ip_address" {
+  value = aws_instance.web.public_ip
+}
+
+
+#DISPLAY FILE SYSTEM ID
+output "FILE_SYSTEM_ID" {
+  value = aws_efs_file_system.efs.id
+}
+
+#DISPLAY MOUNT TARGET DNS NAME
+output "mount_target_dns" {
+  value = aws_efs_file_system.efs.dns_name
+}
