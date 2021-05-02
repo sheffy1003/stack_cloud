7135fb7379a32bcb5580fed3a8cdf18f822c422c
sheriff suara <suara.sheriff@gmail.com>
2021-05-01 22:57:10 -0400

    Initial commit

--- output.tf
+++ output.tf
@@ -10,7 +10,7 @@ output "FILE_SYSTEM_ID" {
   value = aws_efs_file_system.efs.id
 }
 
-#DISPLAY MOUNT TARGET DNS NAME
+#DISPLAY MOUNT TARGET NAME
 output "mount_target_dns" {
   value = aws_efs_file_system.efs.dns_name
 }
