79af4f908b254abcd3293aa95d23501c44a5009b
sheriff suara <suara.sheriff@gmail.com>
2021-05-21 16:02:32 -0400

    update terraform

--- vpc_config.tf
+++ vpc_config.tf
@@ -86,5 +86,20 @@ resource "aws_route_table_association" "attach_private" {
   route_table_id = aws_route_table.main_private.id
 }
 
+data "aws_route53_zone" "selected" {
+  name         = "sheffcloud.com"
+  private_zone = true
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "www.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = data.aws_route53_zone.selected.zone_id
+    evaluate_target_health = true
+  }
+}
 
 
